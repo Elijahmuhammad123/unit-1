293d96bbc6bc68c0c7c7ddfabb6c5d44cf952dc0
Elijahmuhammad123 <831598@mystma.org>
2020-09-17 20:04:31 +0000

    top 10 movies

---
+++ elijahjava/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1.pinneapple express");
+        System.out.println("2.percy jackson");
+        System.out.println("3. avengers");
+        System.out.println("4. avengers 2");
+        System.out.println("5. avengers 3");
+        System.out.println("6. avengers 4");
+        System.out.println("7. harry potter");
+        System.out.println("8. harry potter 2");
+        System.out.println("9.harry potter 3");
+        System.out.println("10.harry potter 4");
+
+    }
+}
