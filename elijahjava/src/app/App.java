0cf6581e4c331516a24be2f467ab221ca3d6d67f
Elijahmuhammad123 <831598@mystma.org>
2020-09-19 22:57:41 +0000

    states in usa

--- elijahjava/src/app/App.java
+++ elijahjava/src/app/App.java
@@ -4,6 +4,11 @@ public class App {
     public static void main(String[] args) throws Exception {
   int myfavnum = 46293; 
 
- system.out.printin(myfavnum);
+
+  System.out.println(myfavnum);
+    
+   byte howmanystatesareintheusa = 50;
+  System.out.println("howmanystatesareintheusa");
+System.out.println(howmanystatesareintheusa);
     }
 }
