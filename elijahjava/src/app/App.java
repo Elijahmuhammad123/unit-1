fd2c053b88b97bd9371c3aca09c80185030b4717
Elijahmuhammad123 <831598@mystma.org>
2020-09-22 16:51:33 +0000

    madlib

--- elijahjava/src/app/App.java
+++ elijahjava/src/app/App.java
@@ -3,34 +3,27 @@ package app;
 public class App {
 
     public static void main(String[] args) throws Exception {
-  int myfavnum = 46293; 
+ String person1 = "zach";
+ String person2 = "kyle";
+ String verb1 = "bat";
+ String verb2 = "car";
+ String verb3 = "plane";
+ String food = "pizza";
+ String dayofweek = "saturday";
+ String place1 = "the zoo";
+ String place2 = "zach's house";
+ String adjective1 = "brave";
+ String adjective2 ="calm";
 
 
-  System.out.println(myfavnum);
-    
-   byte howmanystatesareintheusa = 50;
-  System.out.println("howmanystatesareintheusa");
-System.out.println(howmanystatesareintheusa);
 
-boolean livesinalbertville = false;
-System.out.println("livesinalbertville");
-System.out.println(livesinalbertville);
+ System.out.println(person1 + " is a programmer who likes to " + verb1);
+ System.out.println( dayofweek + " they make a " + adjective1);
+ System.out.println(food + " for breakfast and " + verb2 + " at " + place1 + " with " + person2);
+ System.out.println("Sometimes they go to the " + adjective2 + " " + place2 + " with " + person2 + " so that they can " + verb3 + " together");
 
-char whatdoesmynamestartwith = 30;
-System.out.println("whatdoesmynamestartwith");
-System.out.println(whatdoesmynamestartwith);
 
-short howmanypresidentsarethereintheusa = 45;
-System.out.println("howmanypresidentsarethereintheusa");
-System.out.println(howmanypresidentsarethereintheusa);
-
-int howmanypeopleareontheusa= 328000000;
-System.out.println("howmanypeopleareontheusa");
-System.out.println(howmanypeopleareontheusa);
-
-long howmanyelephantsareintheworld = 400000;
-System.out.println("howmanyelephantsareintheworld");
-System.out.println(howmanyelephantsareintheworld);
+ 
 
     }
 }
