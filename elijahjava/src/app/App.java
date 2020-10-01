eadc65986e342a7cc43f623fed7073b0d185a60d
Elijahmuhammad123 <831598@mystma.org>
2020-10-01 20:17:04 +0000

    quiz 5 questions

--- elijahjava/src/app/App.java
+++ elijahjava/src/app/App.java
@@ -1,22 +1,28 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
-        
-        for (int i = 10; i <= 20; i = i +1){
-System.out.println("the count is "+i);
-for (int l = 10; l <= 100; l = l +1)
-System.out.println("the count is "+l);
-for (int p = 1000; p <= 256000; p = p + 1)
-System.out.println("the count is " + p);
-for (int r = 11; r <= 12; r = r + 1)
-System.out.println("the count is" +r);
-for (int e = 10; e >= 0; e = e + 1)
-System.out.println("the count is " +e);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("what is the color of the sky?");
+        String s1 = input.nextLine();
+
+        if (s1.equals("blue")) {
+            System.out.println("Right");
+
+        } else {
+            System.out.println("wrong");
         }
-
- 
-
+        System.out.println("what color is dirt?");
+        String s2 = input.nextLine();
+if (s2.equals("brown")) {
+    System.out.println("Right");
+}
+else {
+    System.out.println("wrong");
+}
     }
 }
