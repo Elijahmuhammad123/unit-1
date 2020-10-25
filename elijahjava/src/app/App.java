23bcb8ae49ef25a0c74d7540931fae6f03b5d79c
Elijahmuhammad123 <831598@mystma.org>
2020-10-25 23:42:43 +0000

    challenge #4

--- elijahjava/src/app/App.java
+++ elijahjava/src/app/App.java
@@ -7,47 +7,24 @@ public class App {
     public static void main(String[] args) throws Exception {
 
  Scanner input = new Scanner(System.in);
- System.out.println("please enter password.");
- int i = 0;
- String pattern = input.nextLine();
 
- while(i != 3){
-if(pattern.equals("SK8orDie")){
-    System.out.println("you have found the password.");
-    System.exit(1);
-}
-else{
-    System.out.println("wrong password. try again.");
-    i = i + 1;
-}
- }
- System.out.println("please enter a password.");
- String pattern2 = input.nextLine();
+ System.out.println("please enter a number.");
+ int num1 = input.nextInt();
+ input.nextLine();
+ 
+ System.out.println("please enter a larger number");
+ int num2 = input.nextInt();
+ input.nextLine();
 
- if( pattern2.equals("SK8orDie")){
- System.out.println("you have found the password.");
- System.exit(2);    
- }
- else{
-     System.out.println("Wrong password. Try again.");
-     i = i + 1;
- }
- System.out.println("please enter password.");
- String pattern3 = input.nextLine();
+ num1++;
+ while (num1 < num2){
+     if (num1 % 2 == 0){
+         System.out.println(num1);
 
- if (pattern3.equals("SK8orDie")){
-     System.out.println("you have found the password.");
-     System.exit(3);
+         System.out.println(num1);
+     }
+     num1++;
  }
- else {
-     System.out.println("wrong password.");
-     i = i + 1;
- }
- if(i == 3){
-           System.out.println("too many attempts. rerun program.");
-           System.exit(1);
-       }
-input.close();
        }
       
    }
